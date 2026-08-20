308b15fd35bd6471b35a385c66b729dfc630d11d
Ts-Joao <cttjoaovts@gmail.com>
2026-08-19 23:36:18 -0300

    config(api): configura o swagger para documentacao dos endpoints

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DomainExceptionFilter } from './shared/infrastructure/http/filters/domain-exception.filter';
+import { DomainExceptionFilter } from '@shared/infrastructure/http/filters/domain-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Fleet Ops API')
+    .setDescription('Fleet Ops API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
